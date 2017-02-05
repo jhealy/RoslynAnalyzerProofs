132607ff8b4dd5a3eae3babd6a4b34a00e766880
jhealy <josephehealy@hotmail.com>
2017-02-04 16:04:50 -0800

    eightball client and naughty piece added in

---
+++ RoslynProofs/RoslynProofsConsole/EightBallHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoslynProofsConsole.DevfishServices;
+
+namespace RoslynProofsConsole
+{
+    public static class EightBallHelper
+    {
+        public static string AskAQuestion( string question )
+        {
+            using ( EightBallServiceClient client = new EightBallServiceClient() )
+            {
+                return client.AskQuestion(question);
+            }
+        }
+    }
+}
--- RoslynProofs/RoslynProofsConsole/Program.cs
+++ RoslynProofs/RoslynProofsConsole/Program.cs
@@ -10,10 +10,23 @@ namespace RoslynProofsConsole
     {
         static void Main(string[] args)
         {
-            LocalizedData d = new LocalizedData();
-            d.BatteryChargerFailure = "57";
-            d.HighSpeedAlarmMessage = "Failure at 37mbs";
-            Console.WriteLine(d.ToString());
+            // to use the eightball service the eightball service project must be set as a second startup type
+            Console.WriteLine(@"[in rod stewart voice] do you think Im sexxy - ");        
+            string response = EightBallHelper.AskAQuestion(@"do  you think I'm sexxy");
+            if ( String.IsNullOrEmpty(response))
+            {
+                Console.WriteLine("eightball is having issues");
+            }
+            else
+            {
+                Console.WriteLine($"Eightball says {response}");
+            }
+
+
+            //LocalizedData d = new LocalizedData();
+            //d.BatteryChargerFailure = "57";
+            //d.HighSpeedAlarmMessage = "Failure at 37mbs";
+            //Console.WriteLine(d.ToString());
 
             Console.WriteLine(@"Hit enter to continue");
             Console.ReadLine();
