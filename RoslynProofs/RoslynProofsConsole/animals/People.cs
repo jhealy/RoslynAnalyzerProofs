7f5dbd4c0214d9fdbc1db467fd3fdca0f77b76ca
Joe Healy <jhealy@microsoft.com>
2017-02-05 16:08:21 -0800

    CA1819, CA1822, IDE0017

---
+++ RoslynProofs/RoslynProofsConsole/animals/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynProofsConsole.animals
+{
+    public class People
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+}
