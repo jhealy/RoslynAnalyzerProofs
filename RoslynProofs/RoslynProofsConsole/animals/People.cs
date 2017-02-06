1657a4ba3d1a8489f128c9c01d7b2d37a1037377
Joe Healy <jhealy@microsoft.com>
2017-02-05 16:45:38 -0800

    S2239

--- RoslynProofs/RoslynProofsConsole/animals/People.cs
+++ RoslynProofs/RoslynProofsConsole/animals/People.cs
@@ -25,5 +25,9 @@ namespace RoslynProofsConsole.animals
                 targetValue = targetValue.ToLower();
             }
         }
+
+        // S2239
+        public const string CHECKBOX_STRING = "<span class=\"span{7}\" style=\"{0}\"><input type=\"checkbox\" id=\"{7}\" name=\"{1}\" checked=\"{2}\"  tablename=\"{4}\" value=\"{5}\" tabindex=\"{6}\" />&nbsp;<label for=\"{7}\">{3}</label></span>";
+        public const string CHECKBOX_UNCHECK_STRING = "<span class=\"span{6}\" style=\"{0}\"><input type=\"checkbox\" id=\"{6}\" name=\"{1}\" tablename=\"{3}\" value=\"{4}\" tabindex=\"{5}\" />&nbsp;<label for=\"{6}\">{2}</label></span>";
     }
 }
